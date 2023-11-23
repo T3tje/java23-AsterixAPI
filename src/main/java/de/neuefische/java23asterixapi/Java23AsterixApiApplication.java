5ba2bf68827e8b66eaf92eeaae9af895f316935f
T3tje <tilman2013@gmail.com>
2023-11-23 11:49:08 +0100

    initial commit

---
+++ src/main/java/de/neuefische/java23asterixapi/Java23AsterixApiApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.java23asterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java23AsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java23AsterixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/java23asterixapi/Java23AsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.java23asterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java23AsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
